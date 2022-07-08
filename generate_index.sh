87a97372a7c996dfed432afd95aa440b5dcede92
Tom Harrop <twharrop@gmail.com>
2022-07-08 15:20:34 +1000

    readme

--- generate_index.sh
+++ generate_index.sh
@@ -5,19 +5,19 @@
 # Files downloaded from http://aten.reefgenomics.org/download/ and
 # https://espace.library.uq.edu.au/view/UQ:8279c9a/Cladocopium_goreaui.zip?dsi_version=b32c42da081efc36eaa5d8118d322bc6
 zcat \
-	<( unzip -p data/Cladocopium_goreaui.zip \
-	   Cladocopium_goreaui/Cladocopium_goreaui.genome.fa.gz ) \
+    <( unzip -p data/Cladocopium_goreaui.zip \
+       Cladocopium_goreaui/Cladocopium_goreaui.genome.fa.gz ) \
     data/aten_final_0.11.fasta.gz \
     > output/combined.aten_0.11.maker_post_001.Cladocopium_goreaui.fasta
 
 # use rna-star to generate a mapping index
 STAR \
-	--runThreadN 32 \
-	--runMode genomeGenerate \
-	--genomeDir output/index \
-	--genomeFastaFiles output/combined.aten_0.11.maker_post_001.Cladocopium_goreaui.fasta \
-	--sjdbGTFfile output/combined.aten_0.11.maker_post_001.Cladocopium_goreaui.gff \
-	--sjdbOverhang 149 \
-	--sjdbGTFtagExonParentTranscript Parent
+    --runThreadN 32 \
+    --runMode genomeGenerate \
+    --genomeDir output/index \
+    --genomeFastaFiles output/combined.aten_0.11.maker_post_001.Cladocopium_goreaui.fasta \
+    --sjdbGTFfile output/combined.aten_0.11.maker_post_001.Cladocopium_goreaui.gff \
+    --sjdbOverhang 149 \
+    --sjdbGTFtagExonParentTranscript Parent
 
 # you can map against this using STAR --genomeDir output/index etc...
